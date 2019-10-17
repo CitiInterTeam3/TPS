504cc2f9f0b0b8db90ebd073d14806af286e0d37
Geralt-of-China <2627864197@qq.com>
2019-10-17 14:10:41 +0800

    构建项目

---
+++ src/test/java/com/CitiTeam3/TPS/TpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CitiTeam3.TPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
