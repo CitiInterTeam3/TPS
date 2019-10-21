869d4e0d7839004533c6a5fe2602463e2a4723f7
Geralt-of-China <2627864197@qq.com>
2019-10-21 14:33:06 +0800

    完成登陆页面

--- src/main/java/com/CitiTeam3/TPS/controller/HelloWorldController.java
+++ src/main/java/com/CitiTeam3/TPS/controller/HelloWorldController.java
@@ -2,33 +2,64 @@ package com.CitiTeam3.TPS.controller;
 
 import com.CitiTeam3.TPS.dao.TraderDao;
 import com.CitiTeam3.TPS.domain.Trader;
+import com.CitiTeam3.TPS.service.TraderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HelloWorldController {
 
     @Autowired
     TraderDao dao;
+    @Autowired
+    TraderService service;
 
     @RequestMapping("/getTrader")
     public String hello(Model mv, HttpServletRequest request){
         String id=request.getParameter("traderId");
         Trader trader=dao.getTraderById(id);
+        if (trader==null){
+            mv.addAttribute("msg","no such trader");
+            return "error";
+        }
         mv.addAttribute("trader",trader);
         return  "test";
     }
 
-    @RequestMapping("/login")
-    public String login(Model mv, HttpServletRequest request){
-        return  "login";
+    @RequestMapping("/homePage")
+    public String homePage(Model mv, HttpServletRequest request){
+        return  "homePage";
     }
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public String login(HttpServletRequest request){
+        String username=request.getParameter("userName");
+        Trader trader=service.isValid(request.getParameter("userName"),
+                request.getParameter("psw"));
+        if (trader!=null){
+            request.getSession().setAttribute("trader",trader);
+            return "success";
+        }
+        else return "faild";
+    }
+
+    @RequestMapping("/personalHome")
+    public String personHome(Model model,HttpSession session){
+        Object trader=session.getAttribute("trader");
+        if (trader==null){
+            model.addAttribute("msg","session time out");
+            return "error";
+        }else {
+            model.addAttribute("trader",trader);
+            return "personalHome";
+        }
+    }
+
 }
--- src/main/java/com/CitiTeam3/TPS/service/TraderService.java
+++ src/main/java/com/CitiTeam3/TPS/service/TraderService.java
@@ -1,11 +1,24 @@
 package com.CitiTeam3.TPS.service;
 
 import com.CitiTeam3.TPS.dao.TraderDao;
+import com.CitiTeam3.TPS.domain.Trader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TraderService {
     @Autowired
     TraderDao dao;
 
-
+    /**
+     * 根据username和psw判断是否为有效用户，若有效返回相应trader对象，否则返回null
+     * @param userName
+     * @param psw
+     * @return
+     */
+    public Trader isValid(String userName,String psw){
+        Trader trader=dao.getTraderByUserName(userName);
+        if (trader!=null&&!trader.getPsw().equals(psw))return null;
+        else return trader;
+    }
 }
