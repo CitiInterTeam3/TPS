bc8613a24ef347f46c5d952eeb1cc481db9e3327
Geralt-of-China <2627864197@qq.com>
2019-10-22 09:48:18 +0800

    完成添加交易页面,修改了bug

---
+++ src/main/java/com/CitiTeam3/TPS/dao/SalesRepo.java
@@ -0,0 +1,8 @@
+package com.CitiTeam3.TPS.dao;
+
+import com.CitiTeam3.TPS.domain.Sales;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalesRepo extends JpaRepository<Sales,String> {
+
+}
---
+++ src/main/java/com/CitiTeam3/TPS/domain/Sales.java
@@ -0,0 +1,48 @@
+package com.CitiTeam3.TPS.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+public class Sales implements Serializable {
+    @Id
+    private String salesId;
+    private String salesName;
+    private String password;
+
+    public Sales(){}
+    public Sales(String salesId,String salesName,String password){
+        this.salesId=salesId;
+        this.salesName=salesName;
+        this.password=password;
+    }
+
+    public String getSalesId() {
+        return salesId;
+    }
+
+    public void setSalesId(String salesId) {
+        this.salesId = salesId;
+    }
+
+    public String getSalesName() {
+        return salesName;
+    }
+
+    public void setSalesName(String salesName) {
+        this.salesName = salesName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
